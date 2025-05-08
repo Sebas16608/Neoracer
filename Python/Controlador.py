6a2ef0beeecdb7005c0071f561e80a49db7acf57
Sebas16608 <asebasrr444@gmail.com>
2025-05-07 21:18:10 -0600

    Empezamos a hacer las funciones para el controlador del deepracer

---
+++ Python/Controlador.py
@@ -0,0 +1,13 @@
+def izquiera():
+    return "Girar a la izquierda"
+
+def derecha():
+    return "Girar a la derecha"
+
+def recto():
+    return "Debe seguir recto"
+
+def atras():
+    return "Debe retroceder"
+
+
