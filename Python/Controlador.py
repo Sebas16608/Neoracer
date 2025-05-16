ff3823e3f64364918000c933bb2cd445f733be29
Sebas16608 <asebasrr444@gmail.com>
2025-05-16 11:04:56 -0600

    Cree un archivo .html para poder manejar el carrito desde una app web que uhare usando flask y tambien decidi hacer el controlador con POO

--- Python/Controlador.py
+++ Python/Controlador.py
@@ -1,30 +1,3 @@
-def izquiera():
-    return "Girar a la izquierda"
-
-def derecha():
-    return "Girar a la derecha"
-
-def recto():
-    return "Debe seguir recto"
-
-def atras():
-    return "Debe retroceder"
-
-
-carrito = True
-
-#servira para hacer un boton
-while carrito:
-    print("Que deseas hacer? \n 1. Encender \n 2. Apagar")
-    opcion = int(input(""))
-
-    if opcion == 1:
-        print("Encendiendo... \n")
-    
-    elif opcion == 2:
-        print("Okay bye... ")
-        break
-
-    else:
-        print("opcion incorrecta")
-        break
+class carrito:
+    def __init__(self):
+        pass
