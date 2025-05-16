7d4b9d942823ee65386168b86d1b82c2a556de15
Sebas16608 <asebasrr444@gmail.com>
2025-05-16 11:30:00 -0600

    Empece a hacer el estado del carrito

--- Python/Controlador.py
+++ Python/Controlador.py
@@ -1,3 +1,16 @@
 class carrito:
     def __init__(self):
-        pass
+        self.estado = "Detenido"
+
+    def avanzar(self):
+        self.estado = "Avanzar"
+        return "El carrito avanzar"
+    def retroceder(self):
+        self.estado = "Retroceder"
+        return "El carrito retrocede"
+    def izquierda(self):
+        self.estado = "Izquierda"
+        return "El carrito gira a la izquierda"
+    def derecha(self):
+        self.estado = "Derecha"
+        return "El carrito gira a la derecha"
