afdc1efebee2272057080e44349b6100b4c64156
Sebas16608 <asebasrr444@gmail.com>
2025-05-28 18:41:01 -0600

    Cree lo que son los controles y pase la clase al app.py para poder configurar las funciones del carrito

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ app = Flask(__name__)
 
 boostrap = Bootstrap(app)
 
+class Carrito:
+    def __init__(self):
+        self.estado = "Detenido"
+
+    def avanzar(self):
+        self.estado = "Avanzar"
+        return "El carrito está avanzando"
+
+    def retroceder(self):
+        self.estado = "Retroceder"
+        return "El carrito está retrocediendo"
+
+    def izquierda(self):
+        self.estado = "Izquierda"
+        return "El carrito gira a la izquierda"
+
+    def derecha(self):
+        self.estado = "Derecha"
+        return "El carrito gira a la derecha"
+
+    def piloto(self):
+        self.estado = "Piloto Automático"
+        return "Se activó el piloto automático"
 
 @app.route('/')
 def home():
@@ -12,6 +35,10 @@ def home():
 @app.route('/informacion')
 def info():
     return render_template("info.html")
+
+@app.route('/controlador')
+def controlador():
+    return render_template("controlador.html")
 
 
 app.run(host="0.0.0.0", port="1995", debug=True)
