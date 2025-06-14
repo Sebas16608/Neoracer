87308739caa9e453c023f8347920a5859feb995d
Sebas16608 <asebasrr444@gmail.com>
2025-06-14 08:55:00 -0600

    casi terminamos

--- app.py
+++ app.py
@@ -1,32 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 
 boostrap = Bootstrap(app)
-
-class Carrito:
-    def __init__(self):
-        self.estado = "Detenido"
-
-    def avanzar(self):
-        self.estado = "Avanzar"
-        return "El carrito está avanzando"
-
-    def retroceder(self):
-        self.estado = "Retroceder"
-        return "El carrito está retrocediendo"
-
-    def izquierda(self):
-        self.estado = "Izquierda"
-        return "El carrito gira a la izquierda"
-
-    def derecha(self):
-        self.estado = "Derecha"
-        return "El carrito gira a la derecha"
-
-    def piloto(self):
-        self.estado = "Piloto Automático"
-        return "Se activó el piloto automático"
 
 @app.route('/')
 def home():
