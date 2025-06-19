dc7f188a79f95f9dfdb0872002174007f1f47013
Sebas16608 <asebasrr444@gmail.com>
2025-06-18 22:51:05 -0600

    Empezando la web

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template, render_template
 
 app = Flask(__name__)
 
-@app.route("/Index")
+@app.route("/")
 def inicio():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
