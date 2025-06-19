04343e9118ffc8d838597f13f72e41d45501ef90
Sebas16608 <asebasrr444@gmail.com>
2025-06-19 12:21:27 -0600

    cree un archivo .html para almacenar el navbar y luego heredarlo

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/informacion")
+def info():
+    return render_template("info.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
