4fa33b470d08e5738e9c3204da0b97e3bb7c43bb
Sebas16608 <asebasrr444@gmail.com>
2025-06-19 10:00:28 -0600

    Agregando cosas al contenido y a la navbar

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template, render_template
+from flask import Flask, request, render_template, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def inicio():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
